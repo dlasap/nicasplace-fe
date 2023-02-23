b509b569410fa26de6d73c49e565f257502ec263
dom <lasapdominic@gmail.com>
2023-02-23 23:21:46 +0800

    updated overall design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import react from "react";
 import "./styles/Header.css";
-import img from "../assets/logo_nice-transformed-transformed2.png";
+import img from "../assets/new.png";
+// import img from "../assets/logo_nice-transformed.png";
 const Header = () => {
   return (
     <div className="MainHeader">
       <div id="LogoHolder">
-        <img src={img} alt="logo" max-height="50px" width="200px" />
+        <img src={img} alt="logo" max-height="50px" width="125px" />
       </div>
       <div id="MenuOptions">
         <div>Home</div>
